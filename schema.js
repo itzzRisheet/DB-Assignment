8e67a419850958d6bbb56986ffb87f25cbf51b27
itzzRisheet <parmarrisheet29@gmail.com>
2024-03-10 13:25:56 +0530

    chore: Refactor database schema export in schema.js

--- schema.js
+++ schema.js
@@ -4,24 +4,26 @@ const productSchema = Schema({
   name: String,
   desc: String,
   SKU: String,
-  catagory_id: [
-    { type: mongoose.Types.ObjectId, ref: "ProductCategory", required: true },
-  ],
-  inventory_id: [{ type: mongoose.Types.ObjectId, ref: "ProductInventory" }],
+  catagory_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "ProductCategory",
+    required: true,
+  },
+  inventory_id: { type: mongoose.Types.ObjectId, ref: "ProductInventory" },
   price: mongoose.Types.Decimal128,
-  discount_id: [{ type: mongoose.Types.ObjectId, ref: "Discount" }],
+  discount_id: { type: mongoose.Types.ObjectId, ref: "Discount" },
   created_at: Date,
   modified_at: Date,
   deleted_at: Date,
 });
 
 const productCategorySchema = Schema({
-    name : String,
-    desc : String , 
-    created_at : Date , 
-    modified_at : Date ,
-    deleted_at : Date
-})
+  name: String,
+  desc: String,
+  created_at: Date,
+  modified_at: Date,
+  deleted_at: Date,
+});
 
 const productInventorySchema = Schema({
   quantity: Number,
@@ -39,7 +41,6 @@ const discountSchema = Schema({
   modified_at: Date,
   deleted_at: Date,
 });
-
 
 const models = {
   Product: mongoose.model("Product", productSchema),
