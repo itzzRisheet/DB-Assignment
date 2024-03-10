f449e0ed90cde3a054e08c5a3f89a0a24583d90d
itzzRisheet <parmarrisheet29@gmail.com>
2024-03-10 12:14:39 +0530

    chore: Refactor database schema export in models.js

---
+++ schema.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = Schema({
+  name: String,
+  desc: String,
+  SKU: String,
+  catagory_id: [{ type: mongoose.Types.ObjectId, ref: "ProductCategory" }],
+  inventory_id: [{ type: mongoose.Types.ObjectId, ref: "ProductInventory" }],
+  price: mongoose.Types.Decimal128,
+  discount_id: [{ type: mongoose.Types.ObjectId, ref: "Discount" }],
+  created_at: Date,
+  modified_at: Date,
+  deleted_at: Date,
+});
+
+const productCategorySchema = Schema({
+    name : String,
+    desc : String , 
+    created_at : Date , 
+    modified_at : Date ,
+    deleted_at : Date
+})
+
+const productInventorySchema = Schema({
+  quantity: Number,
+  created_at: Date,
+  modified_at: Date,
+  deleted_at: Date,
+});
+
+const discountSchema = Schema({
+  name: String,
+  desc: String,
+  discount_percent: mongoose.Types.Decimal128,
+  active: Boolean,
+  created_at: Date,
+  modified_at: Date,
+  deleted_at: Date,
+});
+
+
+const models = {
+  Product: mongoose.model("Product", productSchema),
+  ProductCategory: mongoose.model("ProductCategory", productCategorySchema),
+  ProductInventory: mongoose.model("ProductInventory", productInventorySchema),
+  Discount: mongoose.model("Discount", discountSchema),
+};
+
+export default models;
